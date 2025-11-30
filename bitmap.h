11b5be6c566d78971cd8ecd13d29739be030e6dd
i.galashin <gis1501@gmail.com>
2025-11-30 21:01:28 +0300

    wer

--- bitmap.h
+++ bitmap.h
@@ -22,6 +22,15 @@ typedef struct {
     uint32_t bfOffBits;    // Смещение до данных изображения
 } BITMAPFILEHEADER;
 
+// struct BITMAPFILEHEADER
+// {
+//     WORD Type; // ‘BM’ 0x4D42
+//     DWORD Size; // Размер файла в байтах, BitCount*Height*Width+ OffsetBits
+//     WORD Reserved1; // Зарезервирован; должен быть нуль
+//     WORD Reserved2; // Зарезервирован; должен быть нуль
+//     DWORD OffsetBits; // Смещение данных от начала файла в байтах// = sizeof(BITMAPFILEHEADER)+sizeof(BITMAPINFOHEADER)
+// };
+
 typedef struct {
     uint32_t biSize;           // Размер структуры
     int32_t  biWidth;          // Ширина изображения
@@ -35,6 +44,21 @@ typedef struct {
     uint32_t biClrUsed;
     uint32_t biClrImportant;
 } BITMAPINFOHEADER;
+
+// struct BITMAPINFOHEADER
+// {
+//     DWORD Size; // Число байтов необходимое для структуры = 40
+//     DWORD Width; // Ширина точечного рисунка в пикселях
+//     DWORD Height; // Высота точечного рисунка в пикселях
+//     WORD Planes; // Число плоскостей целевого устройства = 1
+//     WORD BitCount; // Глубина цвета, число бит на точку = 0,1,4,8,16,24,32
+//     DWORD Compression; // Тип сжатия = 0 для несжатого изображения
+//     DWORD SizeImage; // Размер изображения в байтах BitCount*Height*Width
+//     DWORD XPelsPerMeter; // Разрешающая способность по горизонтали
+//     DWORD YPelsPerMeter; // Разрешающая способность по вертикали
+//     DWORD ColorUsed; // Число индексов используемых цветов. Если все цвета = 0
+//     DWORD ColorImportant; // Число необходимых цветов = 0
+// };
 
 // Структура для хранения одного пикселя (BGR порядок)
 typedef struct {
